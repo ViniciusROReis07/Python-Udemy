3916fd31495e7b803eb032aeb151d7898807d5ff
ViniciusROReis07 <guaguinhireis@gmail.com>
2022-07-22 22:07:45 -0300

    feat: Soma de 3 valores

---
+++ Python3_Intermediario/funcoes/soma_3_numeros.py
@@ -0,0 +1,8 @@
+def soma(n1, n2, n3):
+    print(n1+n2+n3)
+
+numero_1 = float(input("Informe o primeiro numero: "))
+numero_2 = float(input("Informe o segundo numero: "))
+numero_3 = float(input("Informe o terceiro numero: "))
+
+soma(numero_1, numero_2, numero_3)
